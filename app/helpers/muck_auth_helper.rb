439532e5b9fd12bb5dad4388bfbc3d3f91f47a1e
Justin Ball <justinball@gmail.com>
2011-07-12 09:21:10 -0600

    added helper for sign in services

--- app/helpers/muck_auth_helper.rb
+++ app/helpers/muck_auth_helper.rb
@@ -1,11 +1,7 @@
 module MuckAuthHelper
     
   def auth_list(include_icons, services_to_exclude = nil)
-    list = ''
-    remaining_services(services_to_exclude).each do |auth|
-      list << %Q{<li class="#{auth_css_class(auth)} auth_service service-link" #{auth_icon_back(auth, include_icons)} title="#{auth_title(auth)}">#{auth_link(auth)}</li>}
-    end
-    list.html_safe
+    auth_list_html(remaining_services(services_to_exclude), include_icons)
   end
   
   def remaining_services(services_to_exclude = nil)
@@ -19,6 +15,18 @@ module MuckAuthHelper
     services = services.keys.find_all{|key| services[key]['valid_signin']}
     services = services - services_to_exclude.map(&:provider) if services_to_exclude
     services
+  end
+  
+  def signin_list(include_icons, services_to_exclude = nil)
+    auth_list_html(signin_services(services_to_exclude), include_icons)
+  end
+  
+  def auth_list_html(services, include_icons = true)
+    list = ''
+    services.each do |auth|
+      list << %Q{<li class="#{auth_css_class(auth)} auth_service service-link" #{auth_icon_back(auth, include_icons)} title="#{auth_title(auth)}">#{auth_link(auth)}</li>}
+    end
+    list.html_safe
   end
   
   def auth_icon_back(auth, include_icons = true)
